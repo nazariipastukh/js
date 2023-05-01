2d8267bd966b4e78f707e1379443bb3900d423ab
nazariipastukh <nazariy2002n@gmail.com>
2023-05-02 02:09:36 +0300

    first commit -added hw1

---
+++ hw1/hw1.js
@@ -0,0 +1,50 @@
+//-------------------1--------------------//
+
+let a = 'hello'
+let b='owu'
+let c ='com'
+let d = 'ua'
+let e=1
+let f=10
+let g=-999
+let h=123
+let i= 3.14
+let j= 2.7
+let k= 16
+let l= true
+let m= false
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+//-------------------2--------------------//
+
+let firstName = 'Nazarii'
+let middleName='Stepanovych'
+let lastName = 'Pastukh'
+person=`${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+//-------------------3--------------------//
+
+a = 100
+b = '100'
+c = true
+console.log(typeof a, typeof b, typeof c);
+
+//-------------------4--------------------//
+
+let myFirstName=prompt();
+let myMiddleName=prompt();
+let myLastName = prompt();
+console.log(`${myFirstName} ${myMiddleName} ${myLastName}`);
