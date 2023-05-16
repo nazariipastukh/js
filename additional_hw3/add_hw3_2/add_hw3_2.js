6af82b953fe3449d79e9fc95450c6fdaa2cf15ff
nazariipastukh <nazariy2002n@gmail.com>
2023-05-17 01:30:32 +0300

    add hw3_2 commit final commit

--- additional_hw3/add_hw3_2/add_hw3_2.js
+++ additional_hw3/add_hw3_2/add_hw3_2.js
@@ -100,19 +100,27 @@ console.log('--------------------');
 
 // - Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
-//     let usersWithId = [
-//         {id: 1, name: 'vasya', age: 31, status: false},
-//         {id: 2, name: 'petya', age: 30, status: true},
-//         {id: 3, name: 'kolya', age: 29, status: true},
-//         {id: 4, name: 'olya', age: 28, status: false}
-//     ];
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ]
-
+    let usersWithId = [
+        {id: 1, name: 'vasya', age: 31, status: false},
+        {id: 2, name: 'petya', age: 30, status: true},
+        {id: 3, name: 'kolya', age: 29, status: true},
+        {id: 4, name: 'olya', age: 28, status: false}
+    ];
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+]
+let users=usersWithId;
+for (let element of users){
+    for (let adr of citiesWithId){
+        if (adr.user_id===element.id){
+            element.adress=adr;
+        }
+    }
+}
+console.log(users);
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
 // Example:
@@ -130,6 +138,7 @@ console.log('--------------------');
 //             },
 //             // TO BE CONTINUED .....
 //         ]
+console.log('--------------------');
 
 // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
 
