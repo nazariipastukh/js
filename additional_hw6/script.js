cd731b88bf1460c21e3d001c0fb02b9b894c5d56
nazariipastukh <nazariy2002n@gmail.com>
2023-05-24 19:15:20 +0300

    add hw6 commit new commit

--- additional_hw6/script.js
+++ additional_hw6/script.js
@@ -1,23 +1,24 @@
 // - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
 // document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
-let cutArr=[];
 let cutString=(str,n)=>{
-    for (let i=0; i<str.length; i+=n) {
-        cutArr.push(str.substring(i, i+n));
+    let newStr=[];
+    for(let i=0;i<str.length;i+=n){
+        newStr.push(str.slice(i,i+n));
     }
-    return console.log(cutArr);
+    return console.log(newStr);
 }
-cutString('наслаждение',3);
+cutString('насолода', 3);
+
 console.log('--------------------');
 
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
 //     let str = 'Каждый охотник желает знать';
 // document.writeln(delete_characters(str, 7)); // Каждый
 
-let str = 'Каждый охотник желает знать';
-let delete_characters=(str,length)=> console.log(str.substring(0,length));
-delete_characters(str,7);
+let delete_characters=(str,length)=>console.log(str.substring(0,length));
+delete_characters('Кожен мисливець бажає знати',6);
+
 console.log('--------------------');
 
 // - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами.
@@ -25,16 +26,16 @@ console.log('--------------------');
 //     let str = "HTML JavaScript PHP";
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
 
-let str1 = "HTML JavaScript PHP";
+let str = "HTML JavaScript PHP";
 let insert_dash=(str)=>console.log(str.toUpperCase().replaceAll(' ','-'));
-insert_dash(str1);
+insert_dash(str);
+
 console.log('--------------------');
 
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
 
-let string='кожен мисливець бажає знати';
-let upperCase=(strName)=>console.log(strName.charAt(0).toUpperCase()+strName.substring(1));
-upperCase(string);
+let upper=(str)=>console.log(str.charAt(0).toUpperCase()+str.substring(1));
+upper('hello world');
 console.log('--------------------');
 
 // - Дано список імен.
@@ -48,23 +49,54 @@ console.log('--------------------');
 
 
 
+console.log('--------------------');
+
 // - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
 
+let randomGen=(length)=>{
+    let random=[];
+    for (let i=0;i<length;i++){
+        let number=Math.floor(Math.random()*101);
+        random.push(number);
+    }
+    return random;
+}
+let randomArr= randomGen(10);
+console.log(randomArr);
 
+console.log('--------------------');
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
 // Відсортувати його за допомоги sort
 
+let sortNum=(numbers)=>console.log(numbers.sort((a,b)=>a-b));
+sortNum(randomArr);
 
+console.log('--------------------');
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
 // відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
 
+let sortNum0=(numbers)=>console.log(numbers.filter(element=> element!==0 && element%2===0));
+console.log(randomArr);
+sortNum0(randomArr);
 
+console.log('--------------------');
 
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
 
+let capitalize=(str)=>{
+    let words=str.split(' ');
+    let newArr=[];
+    for (let i=0;i<words.length;i++){
+        let word=words[i];
+        words[i]=word.charAt(0).toUpperCase()+word.slice(1);
+    }
+    return words.join(' ');
+}
+console.log(capitalize('hello my name is nazarii'));
 
+console.log('--------------------');
 
 // - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@),
 // наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру
@@ -80,85 +112,99 @@ console.log('--------------------');
 
 
 
-// - є масивlet coursesArray = [
-//     {
-//         title: 'JavaScript Complex',
-//         monthDuration: 5,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-//     },
-//     {
-//         title: 'Java Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'java core',
-//             'java advanced']
-//     },
-//     {
-//         title: 'Python Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'python core',
-//             'python advanced']
-//     },
-//     {
-//         title: 'QA Complex',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-//     },
-//     {
-//         title: 'FullStack',
-//         monthDuration: 7,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'react',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'node.js',
-//             'python',
-//             'java']
-//     },
-//     {
-//         title: 'Frontend',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
-//     }
-// ];
+console.log('--------------------');
+
+// - є масив
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
 // відсортувати його в спадаючому порядку за кількістю елементів в полі modules
 
+let sort=(arrName)=>console.log(arrName.sort((a,b)=>b.modules.length-a.modules.length));
+sort(coursesArray);
 
+console.log('--------------------');
 
 // - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
 //     let symb = "о", str = "Астрономия это наука о небесных объектах";
 // document.writeln(count(str, symb)) // 5
 
-let count=(str,stringsearch)=>console.log(str.split(stringsearch).length-1);
-count("Астрономия это наука о небесных объектах",'о');
+let count=(str,stringsearch)=>{
+    let counter=[];
+    for (let i=0;i<str.length;i++){
+        counter.push(str.slice(i,i+1));
+    }
+    let filter=counter.filter(element=>element===stringsearch);
+    return console.log(filter.length);
+}
+count('Астрономія - це наука про небесні тіла', 'о');
+
 console.log('--------------------');
 
 // - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
@@ -166,6 +212,8 @@ console.log('--------------------');
 // document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
 
 
+
+console.log('--------------------');
 
 // -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
 
@@ -192,59 +240,29 @@ let books = [
 
 // -знайти наібльшу книжку.
 
-let largestBook=(arrName)=>{
-    let sorted=arrName.sort((a,b)=>(b.pageCount-a.pageCount));
-    return console.log(sorted[0]);
-}
-largestBook(books);
+
 console.log('--------------------');
 
 // - знайти книжку/ки з найбільшою кількістю жанрів
 
-let genres=(bookArray)=>{
-    let sortedArr=bookArray.sort((a,b)=>b.genres.length-a.genres.length);
-    for (let element of sortedArr){
-        if (element.genres.length===sortedArr[0].genres.length){
-            console.log(element);
-        }
-    }
-}
-genres(books);
-console.log('--------------------');
 
-// let genres=(arrName)=>arrName.reduce((acc,current)=>{
-//     if (current.genres.length>acc){
-//         return current.genres.length;
-//     }else{
-//         return acc;
-//     }
-// },0);
-// let showBooks=(arrName)=>arrName.filter(element =>element.genres.length===genres(books));
-// console.log(showBooks(books));
-// console.log('--------------------');
+console.log('--------------------');
 
 // - знайти книжку/ки з найдовшою назвою
 
-let title=(bookArray)=>{
-    let sortedArr=bookArray.sort((a,b)=>b.title.length-a.title.length);
-            return console.log(sortedArr[0].title);
-}
-title(books);
+
 console.log('--------------------');
 
 // - знайти книжку/ки які писали 2 автори
 
-let authors=(bookArray)=>console.log(bookArray.filter(element=>element.authors.length===2));
-authors(books);
+
 console.log('--------------------');
 
 // - знайти книжку/ки які писав 1 автор
 
-let authors1=(bookArray)=>console.log(bookArray.filter(element=>element.authors.length===1));
-authors1(books);
+
 console.log('--------------------');
 
 // - вісортувати книжки по кількості сторінок по зростанню
 
-let sort=(arrName)=>console.log(arrName.sort((a, b) => b.pageCount - a.pageCount));
-sort(books);
+console.log('--------------------');
