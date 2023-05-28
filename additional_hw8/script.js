2bd1bdd501cc87dcf67f32ee7c396344f90015ca
nazariipastukh <nazariy2002n@gmail.com>
2023-05-29 01:38:30 +0300

    additional hw8 final commit

--- additional_hw8/script.js
+++ additional_hw8/script.js
@@ -24,7 +24,31 @@
 // }
 // }
 
+class User{
+    constructor(id,name,username,email,address,street,suite,city,zipcode,geo,lat,lng,phone,website,company,nameComp,catchPhrase,bs){
+        this.id=id;
+        this.name=name;
+        this.username=username;
+        this.email=email;
+        this.address={
+            street,suite,city,zipcode,
+            geo:{
+                lat,lng
+            }
+        }
+        this.phone=phone;
+        this.website=website;
+        this.company={
+            nameComp,catchPhrase,bs
+        }
+    }
+}
+console.log(new User(12345,'John Doe','johndoe123','johndoe123@example.com','1 Main Street',
+    'Main Street', 'Apt 101','Anytown',12345,'40.7128° N, 74.0060° W',40.7128,
+    -74.0060,123-456-7890,'www.johndoe123.com','OpenAI Inc.','OpenAI Inc.',
+    "Transforming the future with AI",'Artificial Intelligence solutions'));
 
+console.log('--------------------');
 
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
 // Поля :
@@ -36,7 +60,27 @@
 // -опис дії атрибуту
 // інформацію брати з htmlbook.ru
 
+class Tag{
+    constructor(titleOfArg,action,...attrs){
+        this.titleOfArg=titleOfArg;
+        this.action=action;
+        this.attrs=attrs;
+    }
+}
+class Attr{
+    constructor(titleOfAttr,actionOfAttr){
+        this.titleOfAttr=titleOfAttr;
+        this.actionOfAttr=actionOfAttr;
+    }
+}
+console.log(new Tag('div','Тег використовується, щоб групувати блоки інформації та форматувати її за допомогою CSS',
+    new Attr('align','Визначає вирівнювання вмісту всередині елемента.'),
+    new Attr('title','Вказує додаткову текстову підказку')));
 
+console.log(new Tag('span','Являє собою універсальний порожній контейнер, який необхідно заповнити будь-яким вмістом',
+    new Attr('translate','Повідомляє браузеру переводити вказаний текст в елементі чи ні.'),
+    new Attr('contextmenu','Контекстне меню для елемента'),
+    new Attr('accesskey','Дозволяє активувати або сфокусуватись на елементі за допомогою заданого сполучення клавіш.')));
 
 // Таким чином описати теги
 // -a
