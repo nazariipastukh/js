6fa2d5cd9394f8fa77071d94184c9f3698885392
nazariipastukh <nazariy2002n@gmail.com>
2023-06-04 00:43:11 +0300

    hw 10 commit 5

--- hw10/script_5.js
+++ hw10/script_5.js
@@ -1,3 +1,17 @@
-// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні
+// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript,
+// зробіть так, щоб при натисканні
 // на кнопку зникав елемент з id="text".
+
+let button=document.createElement('button');
+let button1=document.createElement('button');
+let div=document.createElement('div');
+div.setAttribute('id','text');
+document.body.append(button,button1,div);
+
+button1.addEventListener('click',function(e){
+    div.style.display='block';
+});
+button.addEventListener('click',function(e){
+    div.style.display='none';
+});
 
